2ec6b07fe6056bd8b849e127c9a77457bc3623e9
Simon Mac Aree <smacr25@gmail.com>
2023-05-06 11:11:41 +0100

    Adding components
    Adding lost and won banners

---
+++ src/components/LostBanner/LostBanner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LostBanner() {
+  return <div />;
+}
+
+export default LostBanner;
---
+++ src/components/WonBanner/WonBanner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WonBanner() {
+  return <div />;
+}
+
+export default WonBanner;
